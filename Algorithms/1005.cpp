3712360b0708dec60fff545a7135e6f1ddf96cd6
JangMotti <totok682@naver.com>
2018-10-31 21:23:53 +0900

    181031_1_BOJ 알고리즘 공부 Solving: 1005

---
+++ Algorithms/1005.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int buildTime[1001];
+int dp[1001];
+pair<int, int> buildRule[1001];
+
+int getBuildTime(int building_num)
+{
+	
+}
+int main()
+{
+	
+
+}
