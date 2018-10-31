8060ed0acbd60d85d3c3d3d92abbf041aac60386
JangMotti <totok682@naver.com>
2018-10-31 21:52:54 +0900

    181031_2_BOJ 알고리즘 공부 Solving: 1005 Solved: 7287, 10172, 10718, 11718, 11719

--- Algorithms/1005.cpp
+++ Algorithms/1005.cpp
@@ -1,16 +1,68 @@
-#include <iostream>
-
-using namespace std;
-int buildTime[1001];
-int dp[1001];
-pair<int, int> buildRule[1001];
-
-int getBuildTime(int building_num)
-{
-	
-}
-int main()
-{
-	
-
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//
+//int T, N, K, W;
+//int buildTime[1001];
+//int dp[1001];
+//pair<int, int> buildRule[1001];
+//
+//int getBuildTime(int building_num)
+//{
+//	if (building_num == 1)
+//		return buildTime[0];
+//	if (dp[building_num - 1] > 0)
+//		return dp[building_num - 1];
+//	else
+//	{
+//		for (int i = 0; i < K; i++)
+//		{
+//			if (buildRule[i].second == building_num)
+//			{
+//				if (dp[buildRule[i].first] > 0)
+//				{
+//					if (dp[building_num - 1] > 0)
+//					{
+//						if (dp[building_num - 1] < dp[buildRule[i].first] + buildTime[building_num - 1])
+//							dp[building_num - 1] = dp[buildRule[i].first] + buildTime[building_num - 1];
+//					}
+//					else
+//						dp[building_num - 1] = dp[buildRule[i].first] + buildTime[building_num - 1];
+//				}
+//				else
+//				{
+//					dp[buildRule[i].first] = getBuildTime(buildRule[i].first);
+//				}
+//			}
+//		}
+//		return dp[building_num - 1];
+//	}
+//}
+//int main()
+//{
+//	cin >> T;
+//	for (int i = 0; i < T; i++)
+//	{
+//		for (int j = 0; j < 1001; j++)
+//		{
+//			buildTime[j] = -1;
+//			dp[j] = -1;
+//		}
+//
+//		cin >> N >> K;
+//		for (int j = 0; j < N; j++)
+//		{
+//			cin >> buildTime[j];
+//		}
+//		for (int j = 0; j < K; j++)
+//		{
+//			cin >> buildRule[j].first >> buildRule[j].second;
+//		}
+//
+//		cin >> W;
+//
+//		cout << getBuildTime(W);
+//	}
+//
+//}
---
+++ Algorithms/7287.cpp
@@ -0,0 +1,9 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	cout << 22 << endl;
+//	cout << "totok682" << endl;
+//}
