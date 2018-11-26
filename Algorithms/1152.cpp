6025cf3f588245c0beea285fda3820dea3b2899c
JangMotti <totok682@naver.com>
2018-11-26 21:01:32 +0900

    181126_2_BOJ 알고리즘 공부 Solving: 1005, 2448 Solved: 1152

---
+++ Algorithms/1152.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string input;
+//	int wordCount = 0;
+//
+//	getline(cin, input);
+//
+//	if (input[0] == ' ')
+//		wordCount--;
+//	if (input[input.length()-1] != ' ')
+//		wordCount++;
+//
+//	for (int i = 0; i < input.length(); i++)
+//		if (input[i] == ' ')
+//			wordCount++;
+//	
+//	cout << wordCount << '\n';
+//
+//	return 0;
+//}
--- Algorithms/2448.cpp
+++ Algorithms/2448.cpp
@@ -1,8 +1,43 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//bool arrStar[3073][6145] = { false, };
+//
+//void makeTreeArray(int line)
+//{
+//	int treeNum = 1;
+//	for (int i = 1; i < (line / 3); i++)
+//		treeNum *= 3;
+//
+//	for (int i = 0; i < treeNum; i++)
+//	{
+//		arrStar[0][2 + 3 * i] = true;
+//
+//		arrStar[1][1 + 3 * i] = true;
+//		arrStar[1][3 + 3 * i] = true;
+//
+//		arrStar[2][0 + 3 * i] = true;
+//		arrStar[2][1 + 3 * i] = true;
+//		arrStar[2][2 + 3 * i] = true;
+//		arrStar[2][3 + 3 * i] = true;
+//		arrStar[2][4 + 3 * i] = true;
+//	}
+//}
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//	makeTreeArray(N);
+//	for (int i = 0; i < N; i++)
+//	{
+//		for (int j = 0; j < N * 2; j++)
+//		{
+//			if (arrStar[i][j])
+//				cout << '*';
+//			else
+//				cout << ' ';
+//		}
+//		cout << '\n';
+//	}
+//}
