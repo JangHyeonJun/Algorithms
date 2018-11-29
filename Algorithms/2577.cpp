08f442c82b39f4eacdfab1e6cb72272dbc9704cc
JangMotti <totok682@naver.com>
2018-11-29 18:28:17 +0900

    181129_1_BOJ 알고리즘 공부 Solving: 1005 Solved: 2577

---
+++ Algorithms/2577.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	int A, B, C;
+//	int cnt[10] = { 0, };
+//	cin >> A >> B >> C;
+//
+//	string num = to_string(A*B*C);
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		for (int j = 0; j < num.length(); j++)
+//		{
+//			if (num[j] - 48 == i)
+//				cnt[i]++;
+//		}
+//		cout << cnt[i] << '\n';
+//	}
+//
+//	return 0;
+//}
