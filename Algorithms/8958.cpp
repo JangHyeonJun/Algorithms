07cc814c0de45e6a0d1e7be8d2aa036e2d542638
JangMotti <totok682@naver.com>
2018-11-30 19:25:12 +0900

    181130_1_BOJ 알고리즘 공부 Solving: 1005 Solved: 8958

---
+++ Algorithms/8958.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int T;
+//	cin >> T;
+//	for (int i = 0; i < T; i++)
+//	{
+//		string input;
+//		cin >> input;
+//		int score = 0;
+//		int sum = 0;
+//		for (int j = 0; j < input.length(); j++)
+//		{
+//			if (input[j] == 'O')
+//				score++;
+//			else
+//				score = 0;
+//
+//			sum += score;
+//		}
+//		cout << sum << '\n';
+//	}
+//
+//	return 0;
+//}
