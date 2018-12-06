4cf581e3d47db574f2143661812955bd879b603e
JangMotti <totok682@naver.com>
2018-12-06 16:49:37 +0900

    181206_2_BOJ 알고리즘 공부 Solving: 1005 Solved: 5622

---
+++ Algorithms/5622.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string input;
+//	cin >> input;
+//
+//	int time = 0;
+//
+//	for (int i = 0; i < input.length(); i++)
+//	{
+//		if ((int)input[i] < 80)
+//			time += (int)((input[i] - 56) / 3);
+//		else if ((int)input[i] < 87)
+//			time += (int)((input[i] - 48) / 4);
+//		else
+//			time += 10;
+//	}
+//
+//	cout << time << '\n';
+//
+//	return 0;
+//}
