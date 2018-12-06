96854b06616912840df1914f196b352a02c879ef
JangMotti <totok682@naver.com>
2018-12-06 16:06:03 +0900

    181206_1_BOJ 알고리즘 공부 Solving: 1005 Solved: 2908

---
+++ Algorithms/2908.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string A, B;
+//	int revA, revB;
+//	cin >> A >> B;
+//
+//	revA = (A[2] - 48) * 100 + (A[1] - 48) * 10 + (A[0] - 48);
+//	revB = (B[2] - 48) * 100 + (B[1] - 48) * 10 + (B[0] - 48);
+//
+//	if (revA > revB)
+//		cout << revA << '\n';
+//	else
+//		cout << revB << '\n';
+//
+//	return 0;
+//}
