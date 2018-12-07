2654fbf2beba717d02903cf6e43fa32d3db29a66
JangMotti <totok682@naver.com>
2018-12-07 19:37:09 +0900

    181207_1_BOJ 알고리즘 공부 Solving: 1005 Solved: 2292

---
+++ Algorithms/2292.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int input;
+//	int count = 0;
+//	int maxValue = 1;
+//
+//	cin >> input;
+//
+//	while (input > 1)
+//	{
+//		if (input <= maxValue)
+//			break;
+//		count++;
+//		maxValue += count * 6;
+//	}
+//
+//	cout << count + 1 << '\n';
+//
+//	return 0;
+//}
