2acce0521da1ce4dec428f3e1a4b7622501ec203
JangMotti <totok682@naver.com>
2018-12-13 20:05:46 +0900

    181213_1_BOJ 알고리즘 공부 Solving: 1005 Solved: 10250

--- Algorithms/10250.cpp
+++ Algorithms/10250.cpp
@@ -1,9 +1,29 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int T, H, W, N;
+//	int floor, seq;
+//	cin >> T;
+//	for (int k = 0; k < T; k++)
+//	{
+//		cin >> H >> W >> N;
+//
+//		if (N % H != 0)
+//			floor = N % H;
+//		else
+//			floor = H;
+//		seq = (N-1) / H + 1;
+//
+//		if (seq > 9)
+//			cout << floor << seq << '\n';
+//		else
+//			cout << floor << 0 << seq << '\n';
+//	}
+//
+//
+//	return 0;
+//}
