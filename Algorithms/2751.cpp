faed88fc8b77f064345a336f0cde60b930a1d6a7
JangMotti <totok682@naver.com>
2018-12-20 15:54:39 +0900

    181220_1_BOJ 알고리즘 공부 Solving: 1005 Solved: 2751(STL 사용, 이해는 했지만, 한번더 정렬 알고리즘 구현해보기)

---
+++ Algorithms/2751.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//#include <queue>
+//#include <functional>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int N, input, output;
+//	priority_queue<int, vector<int>, greater<int>> pque;
+//
+//	cin >> N;
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		cin >> input;
+//		pque.push(input);
+//	}
+//	for (int i = 0; i < N; i++)
+//	{
+//		cout << pque.top() << '\n';
+//		pque.pop();
+//	}
+//
+//	return 0;
+//}
