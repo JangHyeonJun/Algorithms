4e27f7557b764c4ed0de97bdc456d72c4eafa90a
JangMotti <totok682@naver.com>
2018-12-23 20:50:43 +0900

    181223_1_BOJ 알고리즘 공부 Solving: 1005 Solved: 1427

---
+++ Algorithms/1427.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//#include <queue>
+//using namespace std;
+//
+//int main()
+//{
+//	int N;
+//	int num1;
+//	int num[10] = { 0, };
+//	priority_queue<int> queue;
+//
+//	cin >> N;
+//
+//	while (N > 0)
+//	{
+//		num1 = N % 10;
+//		queue.push(num1);
+//		N = N / 10;
+//	}
+//
+//	while(queue.size() > 0)
+//	{
+//		cout << queue.top();
+//		queue.pop();
+//	}
+//
+//	cout << '\n';
+//
+//	return 0;
+//}
