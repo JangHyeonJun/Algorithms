9705907722bb6528cb2cc5db5eeeab2b9ad514ac
JangMotti <totok682@naver.com>
2019-03-02 19:20:59 +0900

    190302_1_BOJ 알고리즘 공부 Solving: 1005, 1874(중간저장) Solved:

--- Algorithms/1874.cpp
+++ Algorithms/1874.cpp
@@ -1,11 +1,57 @@
 #include<iostream>
 #include<stack>
+#include<queue>
 
 using namespace std;
 
 int main()
 {
-	stack<int> S;
+	stack<int> nums;
+	stack<int> seq;
+	queue<char> output;
+
+	int stackNum;
+	int input[100000];
+
+	cin >> stackNum;
+
+	for (int i = 0; i < stackNum; i++)
+		cin >> input[i];
+
+	for (int i = stackNum; i > 0; i--)
+		nums.push(i);
+
+	for (int i = 0; i < stackNum; i++)
+	{
+		while (!nums.empty() && nums.top() <= input[i])
+		{
+
+			seq.push(nums.top());
+			nums.pop();
+			output.push('+');
+
+		}
+		while (!seq.empty())
+		{
+			if (seq.top() >= input[i])
+			{
+				seq.pop();
+				output.push('-');
+			}
+			else
+			{
+				cout << "NO" << '\0';
+				return 0;
+			}
+		}
+	}
+
+	while (!output.empty())
+	{
+		cout << output.front() << '\n';
+		output.pop();
+	}
+
 
 	return 0;
 }
