3ee62458d23a182f905703797273e41ec61f5b24
JangMotti <totok682@naver.com>
2019-03-09 21:56:23 +0900

    190309_1_BOJ 알고리즘 공부 Solving: 1005 Solved: 1874( 출력에는 정답만 나와야 한다, 널문자 '\0' 도 출력하면 안됨.. 이거 때문에 해맸다 ㅠㅠ )

--- Algorithms/1874.cpp
+++ Algorithms/1874.cpp
@@ -1,70 +1,70 @@
-#include<iostream>
-#include<stack>
-#include<queue>
-
-using namespace std;
-
-int main()
-{
-	stack<int> nums;
-	stack<int> seq;
-	queue<char> output;
-
-	int stackNum;
-	int input[100000];
-	bool error = true;
-
-	cin >> stackNum;
-
-	for (int i = 0; i < stackNum; i++)
-		cin >> input[i];
-
-	for (int i = stackNum; i > 0; i--)
-		nums.push(i);
-
-	for (int i = 0; i < stackNum; i++)
-	{
-		error = true;
-
-		while (!nums.empty() && nums.top() <= input[i])
-		{
-			seq.push(nums.top());
-			nums.pop();
-			output.push('+');
-		}
-		while (!seq.empty())
-		{
-			if (seq.top() == input[i])
-			{
-				seq.pop();
-				output.push('-');
-				error = false;
-				break;
-			}
-			else
-			{
-				seq.pop();
-				output.push('-');
-			}
-		}
-		if (error)
-		{
-			cout << "NO" << '\0';
-			return 0;
-		}
-	}
-
-	if(!output.empty())
-	{
-	cout << output.front();
-	output.pop();
-	}
-	while (!output.empty())
-	{
-		cout << '\n' << output.front();
-		output.pop();
-	}
-
-
-	return 0;
-}
+//#include<iostream>
+//#include<stack>
+//#include<queue>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	stack<int> nums;
+//	stack<int> seq;
+//	queue<char> output;
+//
+//	int stackNum;
+//	int input[100000];
+//	bool error = true;
+//
+//	cin >> stackNum;
+//
+//	for (int i = 0; i < stackNum; i++)
+//		cin >> input[i];
+//
+//	for (int i = stackNum; i > 0; i--)
+//		nums.push(i);
+//
+//	for (int i = 0; i < stackNum; i++)
+//	{
+//		error = true;
+//
+//		while (!nums.empty() && nums.top() <= input[i])
+//		{
+//			seq.push(nums.top());
+//			nums.pop();
+//			output.push('+');
+//		}
+//		while (!seq.empty())
+//		{
+//			if (seq.top() == input[i])
+//			{
+//				seq.pop();
+//				output.push('-');
+//				error = false;
+//				break;
+//			}
+//			else
+//			{
+//				seq.pop();
+//				output.push('-');
+//			}
+//		}
+//		if (error)
+//		{
+//			cout << "NO" << '\n';
+//			return 0;
+//		}
+//	}
+//
+//	if(!output.empty())
+//	{
+//	cout << output.front();
+//	output.pop();
+//	}
+//	while (!output.empty())
+//	{
+//		cout << '\n' << output.front();
+//		output.pop();
+//	}
+//
+//
+//	return 0;
+//}
