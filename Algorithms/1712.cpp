f52c949328cc487b6ed5ed05c18a93b911884e61
JangMotti <totok682@naver.com>
2019-07-03 19:47:05 +0900

    190703_2_BOJ 알고리즘 공부
    나누기, 소수를 사용하지않고 해결하는 방법을 찾아보자, 부동소수점을 사용하면 정확도가 줄어들기 때문에
    또한 반복문 사용을 최소화하기 위해 특정 수를 찾을 때 수식으로만 해결할 수 있는지도 확인하자.
    
    Solving: 1005
    Solved: 1712

---
+++ Algorithms/1712.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//#include<math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int A, B, C;
+//	int point = 0;
+//	cin >> A >> B >> C;
+//
+//	if (B >= C)
+//		cout << -1;
+//	else
+//	{
+//		point = A / (C - B);
+//		cout << (point + 1);
+//	}
+//
+//
+//	return 0;
+//}
