cf2725ce4e4860fe50b1e4f505912f137cc36af5
JangMotti <totok682@naver.com>
2019-07-05 19:19:20 +0900

    190705_1BOJ 알고리즘 공부
    Solving: 1005
    Solved: 11021m 11022, 15596, 10872

---
+++ Algorithms/11021.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int T, A, B;
+//	cin >> T;
+//
+//	for (int i = 0; i < T; i++)
+//	{
+//		cin >> A >> B;
+//		cout << "Case #" << i + 1 << ": " << A + B;
+//
+//		if (i != T - 1)
+//			cout << '\n';
+//	}
+//
+//	return 0;
+//}
---
+++ Algorithms/11022.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int T, A, B;
+//	cin >> T;
+//
+//	for (int i = 0; i < T; i++)
+//	{
+//		cin >> A >> B;
+//		cout << "Case #" << i + 1 << ": " << A
+//			<< " + " << B << " = " << A + B;
+//
+//		if (i != T - 1)
+//			cout << '\n';
+//	}
+//
+//	return 0;
+//}
