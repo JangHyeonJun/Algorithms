40ffdbe0f58fe4dbee117ec4484d4d08524acfe0
JangMotti <totok682@naver.com>
2019-07-05 17:30:02 +0900

    190705_1BOJ 알고리즘 공부
    Solving: 1005
    Solved: 1330, 2753, 2884

---
+++ Algorithms/2884.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int H, M;
+//	int sum = 0;
+//
+//	cin >> H >> M;
+//
+//	sum = H * 60 + M;
+//	sum -= 45;
+//	
+//	if (sum < 0)
+//		sum += 1440;
+//
+//	cout << sum / 60 << ' ' << sum % 60;
+//
+//	return 0;
+//}
