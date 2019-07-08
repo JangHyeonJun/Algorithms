8ec7c2fcb02898e28e1ba1a8d35226e533c011f2
JangMotti <totok682@naver.com>
2019-07-08 17:04:04 +0900

    190708_2 BOJ 알고리즘 공부
    Solving: 1005
    Solved: 3009

---
+++ Algorithms/3009.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	pair<short, short> coord[3];
+//	short x, y;
+//	for (int i = 0; i < 3; i++)
+//		cin >> coord[i].first >> coord[i].second;
+//
+//	if (coord[0].first == coord[1].first)
+//		x = 2;
+//	else if (coord[1].first == coord[2].first)
+//		x = 0;
+//	else
+//		x = 1;
+//
+//	if (coord[0].second == coord[1].second)
+//		y = 2;
+//	else if (coord[1].second == coord[2].second)
+//		y = 0;
+//	else
+//		y = 1;
+//
+//	cout << coord[x].first << ' ' << coord[y].second;
+//
+//	return 0;
+//}
