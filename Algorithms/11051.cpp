3e152a226f2aa792e01af363d4eae18596aac950
JangMotti <totok682@naver.com>
2019-07-30 19:01:50 +0900

    BOJ 알고리즘 공부 Solving: 2565, 11051

--- Algorithms/11051.cpp
+++ Algorithms/11051.cpp
@@ -1,19 +1,24 @@
 //#include <iostream>
 //
-//int bino(const int n, const int k)
-//{
-//	int fact = 1;
-//	for (int i = 2; i <= num; i++)
-//		fact = (fact * i);
-//	return fact;
-//}
+//int dp[1001][1001];
+//
 //
 //int main()
 //{
 //	int n, k;
 //	std::cin >> n >> k;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		for (int j = 0; j <= i; j++)
+//		{
+//			if (i == j || j == 0)
+//				dp[i][j] = 1;
+//			else
+//				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10001;
+//		}
+//	}
 //
-//	std::cout << factorial(n) / (factorial(k)*factorial(n - k));
+//	std::cout << dp[n][k];
 //
 //	return 0;
 //}
