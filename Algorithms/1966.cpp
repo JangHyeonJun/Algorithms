7c374c3b6641c61c3a487757fc38b76673bb73a8
JangMotti <totok682@naver.com>
2019-08-03 18:05:14 +0900

    BOJ 알고리즘 공부 Solving: 2565, 1966 Solved: 2475

--- Algorithms/1966.cpp
+++ Algorithms/1966.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	int t;
-	cin >> t;
-	for (int i = 0; i < t; i++)
-	{
-		int n, m;
-		int levelCount[9];
-
-		cin >> n >> m;
-		int *num = new int[n];
-		for (int j = 0; j < n; j++)
-		{
-			cin >> num[j];
-			levelCount[num[j] - 1]++;
-		}
-	}
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int t;
+//	cin >> t;
+//	for (int i = 0; i < t; i++)
+//	{
+//		int n, m;
+//		int levelCount[9];
+//
+//		cin >> n >> m;
+//		int *num = new int[n];
+//		for (int j = 0; j < n; j++)
+//		{
+//			cin >> num[j];
+//			levelCount[num[j] - 1]++;
+//		}
+//	}
+//}
---
+++ Algorithms/2475.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	long long input[5];
+//	long long sum = 0;
+//	for (int i = 0; i < 5; i++)
+//	{
+//		cin >> input[i];
+//		sum += input[i] * input[i];
+//	}
+//
+//	cout << sum % 10;
+//	
+//
+//	return 0;
+//}
