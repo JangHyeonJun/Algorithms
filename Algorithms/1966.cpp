c77990d9f9d92126f14076fa9f8527747cf51124
JangMotti <totok682@naver.com>
2019-08-04 22:26:37 +0900

    BOJ 알고리즘 공부 Solving: 2565 Solved: 1966, 10866

--- Algorithms/1966.cpp
+++ Algorithms/1966.cpp
@@ -1,4 +1,5 @@
 //#include <iostream>
+//#include <vector>
 //
 //using namespace std;
 //
@@ -9,14 +10,66 @@
 //	for (int i = 0; i < t; i++)
 //	{
 //		int n, m;
-//		int levelCount[9];
-//
+//		vector<int> num;
 //		cin >> n >> m;
-//		int *num = new int[n];
-//		for (int j = 0; j < n; j++)
+//		for (int i = 0; i < n; i++)
 //		{
-//			cin >> num[j];
-//			levelCount[num[j] - 1]++;
+//			int input;
+//			cin >> input;
+//			num.push_back(input);
 //		}
+//
+//		int overNum[10] = { 0, }; // 높은 중요도 갯수
+//		int printCount = 0;
+//		int numLevel = num[m];
+//		int currLevel = numLevel;
+//		for (int i = 0; i < n; i++)
+//			if (num[i] > numLevel)
+//			{
+//				overNum[num[i]]++;
+//				if (currLevel < num[i])
+//					currLevel = num[i];
+//			}
+//		num[m] = -1;
+//		while (currLevel > numLevel)
+//		{
+//			if (overNum[currLevel] < 1)
+//			{
+//				currLevel--;
+//				continue;
+//			}
+//
+//			if (num.front() == currLevel)
+//			{
+//				num.erase(num.begin());
+//				overNum[currLevel]--;
+//				printCount++;
+//			}
+//			else
+//			{
+//				num.push_back(num.front());
+//				num.erase(num.begin());
+//			}
+//
+//		}
+//		while (true)
+//		{
+//			if (num.front() == -1)
+//			{
+//				printCount++;
+//				break;
+//			}
+//			else if (num.front() == numLevel)
+//			{
+//				num.erase(num.begin());
+//				printCount++;
+//			}
+//			else
+//			{
+//				num.push_back(num.front());
+//				num.erase(num.begin());
+//			}
+//		}
+//		cout << printCount << '\n';
 //	}
 //}
