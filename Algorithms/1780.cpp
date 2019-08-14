287feb74c4a446944aa68b9c6ad3800d17a5b03a
JangMotti <totok682@naver.com>
2019-08-14 22:23:47 +0900

    BOJ 알고리즘 공부 Solved: 1780

---
+++ Algorithms/1780.cpp
@@ -0,0 +1,51 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int numPaper(int **paper, int n, int color, int row = 0, int col = 0)
+//{
+//	if (n == 1)
+//	{
+//		if (paper[row][col] == color)
+//			return 1;
+//		else
+//			return 0;
+//	}
+//	else
+//	{
+//		for (int i = 0; i < n; i++)
+//			for (int j = 0; j < n; j++)
+//			{
+//				if (paper[row + i][col + j] != color)
+//					return numPaper(paper, n / 3, color, row, col)
+//					+ numPaper(paper, n / 3, color, row, col + n / 3)
+//					+ numPaper(paper, n / 3, color, row, col + n * 2 / 3)
+//					+ numPaper(paper, n / 3, color, row + n / 3, col)
+//					+ numPaper(paper, n / 3, color, row + n / 3, col + n / 3)
+//					+ numPaper(paper, n / 3, color, row + n / 3, col + n * 2 / 3)
+//					+ numPaper(paper, n / 3, color, row + n * 2 / 3, col)
+//					+ numPaper(paper, n / 3, color, row + n * 2 / 3, col + n / 3)
+//					+ numPaper(paper, n / 3, color, row + n * 2 / 3, col + n * 2 / 3);
+//			}
+//	}
+//	return 1;
+//}
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int **paper = new int*[n];
+//	for (int i = 0; i < n; i++)
+//		paper[i] = new int[n];
+//
+//	for (int i = 0; i < n; i++)
+//		for (int j = 0; j < n; j++)
+//			cin >> paper[i][j];
+//
+//	cout << numPaper(paper, n, -1) << '\n' 
+//		<< numPaper(paper, n, 0) << '\n' 
+//		<< numPaper(paper, n, 1);
+//
+//	return 0;
+//}
