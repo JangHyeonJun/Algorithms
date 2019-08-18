af3d6435f9f6af2fbbc5e59651cece6fa17079e4
JangMotti <totok682@naver.com>
2019-08-18 23:15:24 +0900

    BOJ 알고리즘 공부 Solved: 10816

--- Algorithms/10816.cpp
+++ Algorithms/10816.cpp
@@ -1,22 +1,27 @@
 //#include <iostream>
-//#include <vector>
+//#include <unordered_map>
 //using namespace std;
 //
 //int main()
 //{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
 //	int n, m;
-//
+//	unordered_map<int, int> card;
 //	cin >> n;
-//	vector<int> cardN(n);
 //	for (int i = 0; i < n; i++)
-//		cin >> cardN[i];
-//	
+//	{
+//		int input;
+//		cin >> input;
+//		card[input]++;
+//	}
 //	cin >> m;
-//	vector<int> cardM(m);
-//	for (int i = 0; i < n; i++)
-//		cin >> cardM[i];
-//
-//	qsort(cardN, cardN.size(), );
+//	for (int i = 0; i < m; i++)
+//	{
+//		int input;
+//		cin >> input;
+//		cout << card[input] << ' ';
+//	}
 //
 //	return 0;
 //}
