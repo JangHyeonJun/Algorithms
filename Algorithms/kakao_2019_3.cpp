9e0e2fa1fb9ca0296ea705758d717e4a1558cbd0
JangMotti <totok682@naver.com>
2019-09-06 22:30:55 +0900

    2019 카카오 신입 공채 1차 코딩 테스트  3번문제 풀이중

---
+++ Algorithms/kakao_2019_3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> getSubset(int n)
+{
+	vector<vector<int>> subsetlist;
+	// 아무것도 구분할 수 없는 경우는 제외하므로
+	for (int i = 1; i < (1 << n); i++)
+	{
+		vector<int> subset;
+		for (int j = 0; j < n; j++)
+			if (i & 1 << j)
+			{
+				subset.push_back(j);
+			}
+		subsetlist.push_back(subset);
+	}
+
+	return subsetlist;
+}
+
+int solution(vector<vector<string>> relation) {
+	int answer = 0;
+	int tuple = relation.size();
+	int attribute = relation[0].size();
+	cout << tuple << ' ' << attribute;
+
+	char flag = 0;
+
+	return answer;
+}
+
+int main()
+{
+	vector<vector<string>> relation = { {"100","ryan","music","2"}, {"200","apeach","math","2"},
+	{"300","tube","computer","3"}, {"400","con","computer","4"}, {"500","muzi","music","3"},
+	{"600","apeach","music","2"} };
+
+	solution(relation);
+
+	getSubset(4);
+
+	return 0;
+}
