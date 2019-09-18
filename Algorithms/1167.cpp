03dad85b387fefe3327d4a5b9e5286e0c049ff4a
JangMotti <totok682@naver.com>
2019-09-18 19:25:53 +0900

    BOJ 알고리즘 공부 Solved: 1167

---
+++ Algorithms/1167.cpp
@@ -0,0 +1,66 @@
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//typedef pair<int, int> ii;
+//vector<vector<ii>> graph;
+//vector<bool> visit;
+//
+//ii dfs(int start)
+//{
+//	int maxDist = 0;
+//	int maxNode = start;
+//	visit[start] = true;
+//	for (int i = 0; i < graph[start].size(); i++)
+//	{
+//		int node = graph[start][i].first;
+//		int dist = graph[start][i].second;
+//		if (visit[node])
+//			continue;
+//
+//		visit[node] = true;
+//		ii distAndNode = dfs(node);
+//		dist = dist + distAndNode.first;
+//		if (dist > maxDist)
+//		{
+//			maxDist = dist;
+//			maxNode = distAndNode.second;
+//		}
+//		visit[node] = false;
+//	}
+//	return make_pair(maxDist, maxNode);
+//}
+//
+//int main()
+//{
+//	int v;
+//	cin >> v;
+//	cin.tie(0);
+//	ios::sync_with_stdio(false);
+//
+//	graph.assign(v, vector<ii>());
+//	
+//	for (int i = 0; i < v; i++)
+//	{
+//		int vertex, node, dist;
+//		cin >> vertex;
+//		while(true)
+//		{
+//			cin >> node;
+//			if (node == -1)
+//				break;
+//			cin >> dist;
+//			graph[vertex-1].push_back(make_pair(node-1, dist));
+//		}
+//	}
+//	int max = 0;
+//
+//	visit.assign(v, false);
+//	ii	distAndNode = dfs(0);
+//	visit.assign(v, false);
+//	distAndNode = dfs(distAndNode.second);
+//
+//	cout << distAndNode.first;
+//	return 0;
+//}
