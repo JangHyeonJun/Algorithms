b9b517d2bbb4dcd7adb1ffa4f54114c9badddd3e
JangMotti <totok682@naver.com>
2019-09-25 15:55:31 +0900

    BOJ 알고리즘 공부 Solved: 1012

--- Algorithms/1012.cpp
+++ Algorithms/1012.cpp
@@ -1,93 +1,95 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-const int max_m = 50;
-const int max_n = 50;
-
-int M, N, K;
-
-bool cabbage[max_n][max_m];
-bool visit[max_n][max_m];
-
-void PrintVisit()
-{
-	for (int y = 0; y < N; y++)
-	{
-		for (int x = 0; x < M; x++)
-		{
-			if (visit[y][x])
-				cout << 1;
-			else
-				cout << 0;
-		}
-		cout << '\n';
-	}
-	cout << '\n';
-}
-
-void InitArray()
-{
-	for (int y = 0; y < max_n; y++)
-		for (int x = 0; x < max_m; x++)
-		{
-			cabbage[y][x] = false;
-			visit[y][x] = false;
-		}
-}
-
-bool IsCabbage(int y, int x)
-{
-	bool is_cabbage = false;
-	if (cabbage[y][x] && !visit[y][x])
-	{
-		is_cabbage = true;
-		visit[y][x] = true;
-
-		if (x > 0)
-			IsCabbage(y, x - 1);
-		if (x < M - 1)
-			IsCabbage(y, x + 1);
-		if (y < N - 1)
-			IsCabbage(y + 1, x);
-	}
-	
-	return is_cabbage;
-}
-
-int CountWorms()
-{
-	int count = 0;
-	for(int y=0; y < N; y++)
-		for (int x = 0; x < M; x++)
-		{
-			if (IsCabbage(y, x))
-			{
-				count++;
-				//PrintVisit();
-			}
-		}
-	return count;
-}
-
-int main()
-{
-	cin.tie(0);
-	ios::sync_with_stdio(false);
-	int T;
-	cin >> T;
-	for (int i = 0; i < T; i++)
-	{
-		cin >> M >> N >> K;
-		InitArray();
-		for (int i = 0; i < K; i++)
-		{
-			int X, Y;
-			cin >> X >> Y;
-			cabbage[Y][X] = true;
-		}
-		cout << CountWorms() << '\n';
-	}
-	return 0;
-}
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//const int max_m = 50;
+//const int max_n = 50;
+//
+//int M, N, K;
+//
+//bool cabbage[max_n][max_m];
+//bool visit[max_n][max_m];
+//
+//void PrintVisit()
+//{
+//	for (int y = 0; y < N; y++)
+//	{
+//		for (int x = 0; x < M; x++)
+//		{
+//			if (visit[y][x])
+//				cout << 1;
+//			else
+//				cout << 0;
+//		}
+//		cout << '\n';
+//	}
+//	cout << '\n';
+//}
+//
+//void InitArray()
+//{
+//	for (int y = 0; y < max_n; y++)
+//		for (int x = 0; x < max_m; x++)
+//		{
+//			cabbage[y][x] = false;
+//			visit[y][x] = false;
+//		}
+//}
+//
+//bool IsCabbage(int y, int x)
+//{
+//	bool is_cabbage = false;
+//	if (cabbage[y][x] && !visit[y][x])
+//	{
+//		is_cabbage = true;
+//		visit[y][x] = true;
+//
+//		if (x > 0)
+//			IsCabbage(y, x - 1);
+//		if (x < M - 1)
+//			IsCabbage(y, x + 1);
+//		if (y > 0)
+//			IsCabbage(y - 1, x);
+//		if (y < N - 1)
+//			IsCabbage(y + 1, x);
+//	}
+//	
+//	return is_cabbage;
+//}
+//
+//int CountWorms()
+//{
+//	int count = 0;
+//	for(int y=0; y < N; y++)
+//		for (int x = 0; x < M; x++)
+//		{
+//			if (IsCabbage(y, x))
+//			{
+//				count++;
+//				//PrintVisit();
+//			}
+//		}
+//	return count;
+//}
+//
+//int main()
+//{
+//	cin.tie(0);
+//	ios::sync_with_stdio(false);
+//	int T;
+//	cin >> T;
+//	for (int i = 0; i < T; i++)
+//	{
+//		cin >> M >> N >> K;
+//		InitArray();
+//		for (int i = 0; i < K; i++)
+//		{
+//			int X, Y;
+//			cin >> X >> Y;
+//			cabbage[Y][X] = true;
+//		}
+//		cout << CountWorms() << '\n';
+//	}
+//	return 0;
+//}
--- Algorithms/9251.cpp
+++ Algorithms/9251.cpp
@@ -1,37 +1,37 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-inline int max(int x, int y){ return x > y ? x : y; }
-
-vector<vector<int>> dp(1001, vector<int>(1001, 0));
-
-int LCS(string s1, string s2)
-{
-	for (int i = 1; i <= s1.length(); i++)
-	{
-		for (int j = 1; j <= s2.length(); j++)
-		{
-			if (s1[i-1] == s2[j-1])
-			{
-				dp[i][j] = dp[i - 1][j - 1] + 1;
-			}
-			else
-			{
-				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-			}
-		}
-	}
-
-	return dp[s1.length()][s2.length()];
-}
-
-int main()
-{
-	string s1, s2;
-	cin >> s1 >> s2;
-	cout << LCS(s1, s2);
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//inline int max(int x, int y){ return x > y ? x : y; }
+//
+//vector<vector<int>> dp(1001, vector<int>(1001, 0));
+//
+//int LCS(string s1, string s2)
+//{
+//	for (int i = 1; i <= s1.length(); i++)
+//	{
+//		for (int j = 1; j <= s2.length(); j++)
+//		{
+//			if (s1[i-1] == s2[j-1])
+//			{
+//				dp[i][j] = dp[i - 1][j - 1] + 1;
+//			}
+//			else
+//			{
+//				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//			}
+//		}
+//	}
+//
+//	return dp[s1.length()][s2.length()];
+//}
+//
+//int main()
+//{
+//	string s1, s2;
+//	cin >> s1 >> s2;
+//	cout << LCS(s1, s2);
+//	return 0;
+//}
