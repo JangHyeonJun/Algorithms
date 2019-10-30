6b99e0fc3211a315dee42bb471fb3976a00a6ed3
JangHyeonjun <totok682@naver.com>
2019-10-30 22:40:45 +0900

    BOJ 알고리즘 공부 Solving: 2252(Clever counting, 시간초과)

--- Algorithms/2252.cpp
+++ Algorithms/2252.cpp
@@ -1,57 +1,40 @@
 #include <iostream>
-#include <queue>
-#include <vector>
+
 using namespace std;
-
-const int max_num = 32000;
-
-bool graph[max_num + 1][max_num + 1];
-bool start[max_num + 1];
+const int max_num = 100000;
 bool visit[max_num + 1];
+int G, P;
+
+bool can_docking(int n)
+{
+	for (int i = n; i >= 1; i--)
+		if (!visit[i])
+		{
+			visit[i] = true;
+			return true;
+		}
+	return false;
+}
+
+
 int main()
 {
-	queue<int> q;
-	vector<int> v;
-	int N, M;
-	cin >> N >> M;
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	int count = 0;
+	cin >> G >> P;
 
-	for (int i = 1; i <= N; i++)
-		start[i] = true;
-
-	for (int i = 1; i <= M; i++)
+	for (int i = 1; i <= P; i++)
 	{
-		int A, B;
-		cin >> A >> B;
-		graph[A][B] = true;
-		start[B] = false;
+		int n;
+		cin >> n;
+		if (can_docking(n))
+			count++;
+		else
+			break;
 	}
-	for (int i = 1; i <= N; i++)
-		if (start[i])
-		{
-			q.push(i);
-			visit[i] = true;
-		}
-	
 
-	while (!q.empty())
-	{
-		int node = q.front();
-		q.pop();
-		v.push_back(node);
-		for (int i = 1; i <= N; i++)
-			if (!visit[i] && graph[node][i])
-			{
-				q.push(i);
-				visit[i] = true;
-			}
-	}
-	for (int i = 1; i <= N; i++)
-		if (!visit[i])
-			v.push_back(i);
-
-	for (auto& iter : v)
-		cout << iter << ' ';
-
+	cout << count;
 
 	return 0;
 }
