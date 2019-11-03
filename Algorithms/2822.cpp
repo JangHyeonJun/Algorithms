9770522617e441216594c7d4984685e4b9b9b698
JangHyeonjun <totok682@naver.com>
2019-11-03 21:16:57 +0900

    BOJ 알고리즘 공부 Solving: 2822

---
+++ Algorithms/2822.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int score[8];
+	int min_index[3] = { -1,-1,-1 };
+	int min = 200;
+	for (int i = 0; i < 8; i++)
+		cin >> score[i];
+	for (int i = 0; i < 8; i++)
+		if (score[i] < min)
+		{
+			min_index[0] = i;
+		}
+	min = 200;
+	for (int i = 0; i < 8; i++)
+		if (score[i] < min && i != min_index[0])
+		{
+			min_index[1] = i;
+		}
+	min = 200;
+	for (int i = 0; i < 8; i++)
+		if (score[i] < min && i != min_index[0] && i != min_index[1])
+		{
+			min_index[2] = i;
+		}
+
+	for (int i = 1; i <= 8; i++)
+	{
+		bool flag = false;
+		for (int j = 0; j < 3; j++)
+			if (i == min_index[j] + 1)
+				flag = true;
+
+		if (!flag)
+			cout << i << ' ';
+	}
+
+
+	return 0;
+}
