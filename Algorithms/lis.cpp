5455809d7904ed3491e5e79610d28860d81fc7f2
JangHyeonjun <totok682@naver.com>
2019-11-20 14:40:25 +0900

    최장 증가 부분 수열 알고리즘 업데이트

--- Algorithms/lis.cpp
+++ Algorithms/lis.cpp
@@ -4,6 +4,44 @@
 //
 //using namespace std;
 //
+//vector<int> lis_nlogn_arr(vector<int> arr)
+//{
+//	vector<int> sub_arr;
+//	vector<int> sub_len;
+//	sub_arr.push_back(arr.front());
+//	sub_len.push_back(1);
+//	for (int i = 1; i < arr.size(); i++)
+//	{
+//		if (sub_arr.back() < arr[i])
+//		{
+//			sub_arr.push_back(arr[i]);
+//			sub_len.push_back(sub_arr.size());
+//		}
+//		else
+//		{
+//			auto iter = lower_bound(sub_arr.begin(), sub_arr.end(), arr[i]);
+//			*iter = arr[i];
+//			sub_len.push_back(iter - sub_arr.begin() + 1);
+//		}
+//	}
+//
+//
+//	int size = sub_arr.size();
+//	int i = arr.size() - 1;
+//	vector<int> result_arr(size);
+//	while (size > 0 && i >= 0)
+//	{
+//		if (sub_len[i] == size)
+//		{
+//			result_arr[size - 1] = arr[i];
+//			size--;
+//		}
+//		i--;
+//	}
+//
+//	return result_arr;
+//}
+//
 //int lis_nlogn(vector<int> arr)
 //{
 //	vector<int> sub_arr;
