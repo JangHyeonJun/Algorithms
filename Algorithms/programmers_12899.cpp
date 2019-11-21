853308999105931110654f435c39bb11a6f59900
JangHyeonjun <totok682@naver.com>
2019-11-22 01:15:15 +0900

    프로그래머스 124 나라의 숫자 풀이 완료

---
+++ Algorithms/programmers_12899.cpp
@@ -0,0 +1,29 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//string solution(int n) {
+//	string answer = "";
+//	while(n > 0)
+//	{
+//		int remainder = n % 3;
+//		n /= 3;
+//		if (remainder == 0)
+//		{
+//			answer = "4" + answer;
+//			n--;
+//		}
+//		else if (remainder == 2)
+//			answer = "2" + answer;
+//		else if (remainder == 1)
+//			answer = "1" + answer;
+//	}
+//
+//	return answer;
+//}
+//
+//int main()
+//{
+//	string x = solution(3);
+//}
