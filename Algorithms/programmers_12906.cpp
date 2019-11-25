615d96ca9fba7484aeefbbf0b6b8d298ec0811b2
JangHyeonjun <totok682@naver.com>
2019-11-25 14:51:47 +0900

    프로그래머스 - 같은 숫자는 싫어 - 올바른 괄호

---
+++ Algorithms/programmers_12906.cpp
@@ -0,0 +1,24 @@
+//#include <vector>
+//#include <iostream>
+//
+//using namespace std;
+//
+//vector<int> solution(vector<int> arr)
+//{
+//	vector<int> answer;
+//	int prev = -1;
+//
+//	for (int n : arr)
+//	{
+//		if (n != prev)
+//		{
+//			answer.push_back(n);
+//			prev = n;
+//		}
+//	}
+//
+//	// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+//	cout << "Hello Cpp" << endl;
+//
+//	return answer;
+//}
