c79231245137bf62aa394c62738b0ed71dc84e2a
JangHyeonjun <totok682@naver.com>
2019-11-29 01:24:26 +0900

    BOJ 알고리즘 공부 Reviewed : 1541

--- Algorithms/1541.cpp
+++ Algorithms/1541.cpp
@@ -1,64 +1,64 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
-int main()
-{
-	int sum = 0;
-	int last = 0;
-	bool is_minus = false;
-	string input;
-	cin >> input;
-	
-	auto iter = input.begin();
-	auto prev = iter;
-
-	if (input[0] == '-')
-	{
-		is_minus = true;
-		iter++;
-		prev++;
-	}
-	
-	while (iter != input.end())
-	{
-		if (is_minus)
-		{
-			if (*iter == '-' || *iter == '+')
-			{
-				int a = stoi(input.substr(prev - input.begin(), iter - prev));
-				sum -= stoi(input.substr(prev - input.begin(), iter - prev));
-				iter++;
-				prev = iter;
-			}
-			else
-				iter++;
-		}
-		else
-		{
-			if (*iter == '-' || *iter == '+')
-			{
-				if (*iter == '-')
-					is_minus = true;
-				int a = stoi(input.substr(prev - input.begin(), iter - prev));
-				sum += stoi(input.substr(prev - input.begin(), iter - prev));
-				iter++;
-				prev = iter;
-				
-			}
-			else
-				iter++;
-		}
-	}
-	if (is_minus)
-		sum -= stoi(input.substr(prev - input.begin(), iter - prev));
-	else
-		sum += stoi(input.substr(prev - input.begin(), iter - prev));
-
-	
-	cout << sum;
-
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int sum = 0;
+//	int last = 0;
+//	bool is_minus = false;
+//	string input;
+//	cin >> input;
+//	
+//	auto iter = input.begin();
+//	auto prev = iter;
+//
+//	if (input[0] == '-')
+//	{
+//		is_minus = true;
+//		iter++;
+//		prev++;
+//	}
+//	
+//	while (iter != input.end())
+//	{
+//		if (is_minus)
+//		{
+//			if (*iter == '-' || *iter == '+')
+//			{
+//				int a = stoi(input.substr(prev - input.begin(), iter - prev));
+//				sum -= stoi(input.substr(prev - input.begin(), iter - prev));
+//				iter++;
+//				prev = iter;
+//			}
+//			else
+//				iter++;
+//		}
+//		else
+//		{
+//			if (*iter == '-' || *iter == '+')
+//			{
+//				if (*iter == '-')
+//					is_minus = true;
+//				int a = stoi(input.substr(prev - input.begin(), iter - prev));
+//				sum += stoi(input.substr(prev - input.begin(), iter - prev));
+//				iter++;
+//				prev = iter;
+//				
+//			}
+//			else
+//				iter++;
+//		}
+//	}
+//	if (is_minus)
+//		sum -= stoi(input.substr(prev - input.begin(), iter - prev));
+//	else
+//		sum += stoi(input.substr(prev - input.begin(), iter - prev));
+//
+//	
+//	cout << sum;
+//
+//	return 0;
+//}
