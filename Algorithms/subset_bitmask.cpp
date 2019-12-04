812f1862c0844a0c2597f921c91e193ba84bc7b7
JangHyeonjun <totok682@naver.com>
2019-12-05 01:21:14 +0900

    subset bitmask implementation

---
+++ Algorithms/subset_bitmask.cpp
@@ -0,0 +1,22 @@
+//#include <vector>
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int b;
+//	vector<int> v = { 1,2,3,4 };
+//	for (int i = 0; i < 16; i++)
+//	{
+//		b = 1;
+//		for (int j = 0; j < v.size(); j++)
+//		{
+//			if (i & b)
+//				cout << v[j] << ' ';
+//			b = b << 1;
+//		}
+//		cout << '\n';
+//	}
+//
+//	return 0;
+//}
