cfee1622c14620139f87f73721debc5f85386d97
JangMotti <totok682@naver.com>
2019-12-14 19:31:21 +0900

    프로그래머스 - 줄 서는 방법

--- Algorithms/programmers_12936.cpp
+++ Algorithms/programmers_12936.cpp
@@ -1,59 +1,48 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <iostream>
-using namespace std;
-
-void print(int n, long long k)
-{
-	vector<int> v;
-	for (int i = 1; i <= n; i++)
-		v.push_back(i);
-	do {
-		k--;
-		for (int n : v)
-			cout << n << ' ';
-		cout << endl;
-	} while (next_permutation(v.begin(), v.end()) && k > 1);
-}
-
-vector<int> solution(int n, long long k)
-{
-	vector<int> answer;
-	vector<bool> used(n + 1, false);
-	int max_case = 1;
-	int num = 1;
-	int multiply_num = 1;
-	for (; multiply_num < n; multiply_num++)
-		max_case *= multiply_num;
-	multiply_num--;
-	while (true)
-	{
-		if (answer.size() == n)
-			break;
-		if (k > max_case && num < n)
-		{
-			num++;
-			k -= max_case;
-		}
-		else
-		{
-			if (!used[num])
-			{
-				answer.push_back(num);
-				used[num] = true;
-				if(multiply_num != 0)
-					max_case /= multiply_num--;
-				num = 1;
-			}
-			else
-				num++;
-		}
-	}
-	return answer;
-}
-
-int main()
-{
-	print(6, 25);
-}
+﻿//#include <string>
+//#include <vector>
+//using namespace std;
+//
+//vector<int> solution(int n, long long k)
+//{
+//	vector<int> answer;
+//	vector<int> nums;
+//	long long max_case = 1;
+//	int multiply_num = 1;
+//	for (; multiply_num < n; multiply_num++)
+//		max_case *= multiply_num;
+//	for (int i = 1; i <= n; i++)
+//		nums.push_back(i);
+//	multiply_num--;
+//
+//	while (!nums.empty())
+//	{
+//		if (k > max_case && multiply_num > 0)
+//		{
+//			long long quotient = k / max_case;
+//			long long remainder = k % max_case;
+//			if (remainder == 0)
+//				quotient--;
+//			auto iter = nums.begin() + quotient;
+//			answer.push_back(*iter);
+//			nums.erase(iter);
+//			k -= quotient * max_case;
+//		}
+//		else
+//		{
+//			answer.push_back(nums.front());
+//			nums.erase(nums.begin());
+//		}
+//		
+//		if (multiply_num > 0)
+//		{
+//			max_case /= multiply_num;
+//			multiply_num--;
+//		}
+//	}
+//	return answer;
+//}
+//
+//int main()
+//{
+//	solution(3, 4);
+//}
