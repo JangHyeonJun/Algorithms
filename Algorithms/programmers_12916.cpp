54a2a8778581e1444a106eaf481fdaceef87c70f
JangHyeonjun <totok682@naver.com>
2019-12-28 16:11:23 +0900

    프로그래머스 - 문자열 내 p와 y의 개수 - 문자열 내림차순으로 배치하기 - 문자열 다루기 기본

---
+++ Algorithms/programmers_12916.cpp
@@ -0,0 +1,20 @@
+//#include <string>
+//#include <iostream>
+//using namespace std;
+//
+//bool solution(string s)
+//{
+//	bool answer = true;
+//	int p_count = 0;
+//	int y_count = 0;
+//	for (int i = 0; i < s.length(); i++)
+//	{
+//		s[i] = tolower(s[i]);
+//		if (s[i] == 'p')
+//			p_count++;
+//		else if (s[i] == 'y')
+//			y_count++;
+//	}
+//
+//	return p_count == y_count;
+//}
---
+++ Algorithms/programmers_12918.cpp
@@ -0,0 +1,7 @@
+//#include <string>
+//using namespace std;
+//
+//bool solution(string s) {
+//	for (char c : s) if (c < '0' || c > '9') return false;
+//	return s.length() == 4 || s.length() == 6;
+//}
