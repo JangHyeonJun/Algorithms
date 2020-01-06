ed1bf084986068ad42109b4057fc14ef710be8fd
JangMotti <totok682@naver.com>
2020-01-06 19:33:32 +0900

    프로그래머스 - 자물쇠와 열쇠

--- Algorithms/programmers_60059.cpp
+++ Algorithms/programmers_60059.cpp
@@ -3,55 +3,18 @@
 //#include <iostream>
 //using namespace std;
 //
-//vector<vector<bool>> findLockMat(const vector<vector<int>>& lock)
-//{
-//	int max_row, max_col, min_row, min_col;
-//	max_row = max_col = -1;
-//	min_row = min_col = lock.size();
 //
+//bool canUnlock(const vector<vector<int>>& lock)
+//{
 //	for (int i = 0; i < lock.size(); i++)
 //		for (int j = 0; j < lock.size(); j++)
 //			if (lock[i][j] == 0)
-//			{
-//				if (i > max_row)
-//					max_row = i;
-//				if (i < min_row)
-//					min_row = i;
-//				if (j > max_col)
-//					max_col = j;
-//				if (j < min_col)
-//					min_col = j;
-//			}
-//
-//	int max_diff = max_row - min_row > max_col - min_col ?
-//		max_row - min_row + 1 : max_col - min_col + 1;
-//	if (max_row - min_row > max_col - min_col)
-//	{
-//		max_diff = max_row - min_row;
-//		if (max_col - max_diff >= 0)
-//			min_col = max_col - max_diff;
-//	}
-//	else
-//	{
-//		max_diff = max_col - min_col;
-//		if (max_row - max_diff >= 0)
-//			min_row = max_row - max_diff;
-//	}
-//	max_diff++;
-//
-//	vector<vector<bool>> mat(max_diff, vector<bool>(max_diff, false));
-//
-//	for (int i = 0; i < max_diff; i++)
-//		for (int j = 0; j < max_diff; j++)
-//			if (lock[min_row + i][min_col + j] == 1)
-//				mat[i][j] = true;
-//
-//	return mat;
+//				return false;
+//	return true;
 //}
-//
-//vector<vector<vector<bool>>> getRotatedMat(const vector<vector<bool>>& mat)
+//vector<vector<vector<bool>>> getRotatedKey(const vector<vector<int>>& key)
 //{
-//	int size = mat.size();
+//	int size = key.size();
 //	vector<vector<vector<bool>>> rotated(4,
 //		vector<vector<bool>>(size, vector<bool>(size, false)));
 //
@@ -59,7 +22,7 @@
 //	{
 //		for (int j = 0; j < size; j++)
 //		{
-//			if (mat[i][j] == 1)
+//			if (key[i][j] == 1)
 //			{
 //				rotated[0][i][j] = true;
 //				rotated[1][j][size - 1 - i] = true;
@@ -72,67 +35,98 @@
 //	return rotated;
 //}
 //
-//void printRotatedMat(const vector<vector<vector<bool>>>& mat)
+//void printdMat(const vector<vector<int>>& mat)
 //{
 //	for (int i = 0; i < mat.size(); i++)
 //	{
 //		for (int j = 0; j < mat[0].size(); j++)
 //		{
-//			for (int k = 0; k < mat[0].size(); k++)
-//			{
-//				if (mat[i][j][k])
-//					cout << '1';
-//				else
-//					cout << '0';
-//			}
-//			cout << '\n';
+//			cout << mat[i][j];
 //		}
-//
-//		cout << "\n-------------\n";
+//		cout << "\n";
 //	}
+//	cout << "\n\n\n";
 //}
 //
-//bool canUnlock(const vector<vector<int>>& key, const vector<vector<vector<bool>>> mat)
+//bool moveKey(const vector<vector<bool>>& key, const vector<vector<int>>& lock)
 //{
-//	int count = key.size() - mat[0].size() + 1;
-//	for (int i = 0; i < count; i++)
+//	const int size = lock.size() * 3;
+//	for (int i = 0; i < size; i++)
 //	{
-//		for (int j = 0; j < count; j++)
+//		for (int j = 0; j < size; j++)
 //		{
-//			bool is_same = true;
-//			for (int k = 0; k < 4; k++)
-//			{
-//				is_same = true;
-//				for (int m = 0; m < mat[0].size(); m++)
+//			vector<vector<int>> mat(size, vector<int>(size, 0));
+//			bool unlock = true;
+//
+//			for (int m = 0; m < lock.size(); m++)
+//				for (int n = 0; n < lock.size(); n++)
+//					mat[lock.size() + m][lock.size() + n] = lock[m][n];
+//
+//			for (int m = 0; m < key.size(); m++)
+//				for (int n = 0; n < key.size(); n++)
 //				{
-//					if (!is_same)
-//						break;
-//					for (int n = 0; n < mat[0].size(); n++)
-//					{
-//						if (!is_same)
-//							break;
-//						if (key[i + m][j + n] != 1 && mat[k][m][n])
-//							is_same = false;
-//						else if (key[i + m][j + n] != 0 && !mat[k][m][n])
-//							is_same = false;
-//					}
+//					if (i + m < size && j + n < size && mat[i + m][j + n] == 0 && key[m][n] == 1)
+//						mat[i + m][j + n] = 1;
+//					else if (i + m < size && j + n < size && mat[i + m][j + n] == 1 && key[m][n] == 1)
+//						mat[i + m][j + n] = 0;
 //				}
-//				if (is_same)
-//					return true;
-//			}
+//
+//			for (int m = 0; m < lock.size(); m++)
+//				for (int n = 0; n < lock.size(); n++)
+//					if (mat[lock.size() + m][lock.size() + n] == 0)
+//						unlock = false;
+//
+//			//printdMat(mat);
+//			if (unlock)
+//				return true;
 //		}
 //	}
+//	vector<vector<int>> mat(size, vector<int>(size, 0));
 //	return false;
 //}
 //
+//
 //bool solution(vector<vector<int>> key, vector<vector<int>> lock)
 //{
-//	return canUnlock(key, getRotatedMat(findLockMat(lock)));;
+//	vector<vector<vector<bool>>> rotated_key = getRotatedKey(key);
+//	for (int i = 0; i < rotated_key.size(); i++)
+//		if (moveKey(rotated_key[i], lock))
+//			return true;
+//
+//	return false;
 //}
 //
 //int main()
 //{
-//	vector<vector<int>> key = { {0,0,0}, {0,0,0}, {0,1,1} };
-//	vector<vector<int>> lock = { {0,0,1},{0,0,1},{0,0,0} };
-//	cout << solution(key, lock);
+//	vector<vector<int>> key = { {1,0,1}, {0,1,1}, {0,1,0} };
+//	vector<vector<int>> lock = { {1,1,0}, {1,0,0}, {1,0,1} };
+//
+//	vector<vector<int>> key2 = { {0,0,0}, {1,0,0}, {0,1,1} };
+//	vector<vector<int>> lock2 = { {1,1,1}, {1,1,0}, {1,0,1} };
+//
+//	vector<vector<int>> key3 = { {0,1},{0,1} };
+//	vector<vector<int>> lock3 = { {1,0,1},{0,1,1},{1,1,1} };
+//
+//	vector<vector<int>> key4 = { {0,0,0}, {1,0,0}, {0,1,1} };
+//	vector<vector<int>> lock4 = { {1,1,1}, {1,1,0}, {1,0,1} };
+//
+//	vector<vector<int>> lock_t = { {1,1,1},{1,1,1},{1,1,1} };
+//
+//	vector<vector<int>> key5 = { {0,0,0}, {0,0,0}, {0,0,1} };
+//	vector<vector<int>> lock5 = { {1,1,1,1},{1,1,0,1},{1,1,1,1},{1,1,1,1} };
+//
+//	vector<vector<int>> key6 = { {0,1,0}, {1,1,0}, {0,0,0} };
+//	vector<vector<int>> lock6 = { {1,1,1}, {1,0,0}, {1,0,1} };
+//
+//	vector<vector<int>> key7 = { {0,0,0,0}, {1,1,0,0}, {1,1,0,0}, {0,0,0,0} };
+//	vector<vector<int>> lock7 = { {1,1,1,1,1}, {1,1,1,0,0}, {1,1,1,0,0}, {1,1,1,1,1}, {1,1,1,1,1} };
+//
+//	//cout << solution(key, lock) << endl;
+//	//cout << solution(key2, lock2) << endl;
+//	//cout << solution(key3, lock3) << endl;
+//	//cout << solution(key4, lock4) << endl;
+//	//cout << solution(key4, lock_t) << endl;
+//	//cout << solution(key5, lock5) << endl;
+//	cout << solution(key6, lock6) << endl;
+//	//cout << solution(key7, lock7) << endl;
 //}
