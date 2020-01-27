82ef61e29f596dbcc208c741992481b4b4a0e8ac
JangMotti <totok682@naver.com>
2020-01-27 22:40:20 +0900

    Recursive Adder Implementation

---
+++ Algorithms/recursive_100.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int cumulative_sum(const int n, int sum = 0)
+//{
+//	if (n == 0)
+//		return sum;
+//	else
+//		return cumulative_sum(n - 1, sum + n);
+//}
+//
+//int main()
+//{
+//	cout << cumulative_sum(100) << endl;
+//}
