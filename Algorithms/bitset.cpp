74164bc6316124c0f1e6bf3187762f5779b8511e
JangMotti <totok682@naver.com>
2020-01-27 21:04:08 +0900

    Bitset Implementation

---
+++ Algorithms/bitset.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <bitset>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	bitset<100> bs;
+//	
+//
+//	cout << "bitset<100> size : " << bs.size() << endl;
+//
+//	bs.set(30);
+//	cout << "bs.set(30)" << endl;
+//	cout << "bitset<100> : " << bs << endl;
+//
+//	bs[40] = 1;
+//	cout << "bs[40] = 1" << endl;
+//	cout << "bitset<100> : " << bs << endl;
+//
+//	return 0;
+//}
