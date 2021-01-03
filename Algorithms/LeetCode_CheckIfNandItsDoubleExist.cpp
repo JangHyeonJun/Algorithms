442013c0f8901fa3af0fb117b260a1767aab3292
JangHyoenJun <totok682@naver.com>
2021-01-03 23:58:29 +0900

    Check If N and Its Double Exist 중간 저장

---
+++ Algorithms/LeetCode_CheckIfNandItsDoubleExist.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        const size_t max = 10 * 10 * 10;
+        const size_t size = max * 8;
+
+        bitset<size> set;
+
+        for (const auto& num : arr)
+        {
+            auto index = num + max * 2;
+            set[index] = true;
+        }
+
+        for (const auto& num : arr)
+        {
+            auto doubleIndex = (num * 2) + max * 2;
+            if (set[doubleIndex])
+                return true;
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    Solution s;
+    s.checkIfExist(vector<int>{10, 2, 5, 3});
+}
