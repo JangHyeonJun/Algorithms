2afc6b926f5599a68b3b626aa7427f3790f2304e
JangHyoenJun <totok682@naver.com>
2021-01-10 12:38:58 +0900

    LeetCode - Sort Array By Parity

--- Algorithms/LeetCode_SortArrayByParity.cpp
+++ Algorithms/LeetCode_SortArrayByParity.cpp
@@ -1,45 +1,45 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-class Solution {
-public:
-    vector<int> sortArrayByParity(vector<int>& A) {
-        int evenIndex = 0;
-        int oddIndex = 1;
-
-        while (evenIndex < A.size() && oddIndex < A.size())
-        {
-            if (A[evenIndex] % 2 != 0 && A[oddIndex] % 2 == 0)
-            {
-                Swap(A[evenIndex], A[oddIndex]);
-                evenIndex++;
-                oddIndex++;
-            }
-            else if (A[oddIndex] % 2 != 0)
-            {
-                oddIndex++;
-            }
-            else if (A[evenIndex] % 2 == 0)
-            {
-                evenIndex++;
-            }
-        }
-
-        return A;
-    }
-
-    void Swap(int& a, int& b)
-    {
-        int temp = a;
-        a = b;
-        b = temp;
-    }
-};
-
-int main()
-{
-    Solution s;
-    s.sortArrayByParity(vector<int> {0, 2, 1});
-}
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//class Solution {
+//public:
+//    vector<int> sortArrayByParity(vector<int>& A) {
+//        int evenIndex = 0;
+//        int oddIndex = 0;
+//
+//        
+//
+//        while (oddIndex < A.size())
+//        {
+//            while (oddIndex < A.size() && A[oddIndex] % 2 == 0)
+//                oddIndex++;
+//
+//            evenIndex = oddIndex + 1;
+//
+//            while (evenIndex < A.size() && A[evenIndex] % 2 == 1)
+//                evenIndex++;
+//
+//            if (oddIndex < A.size() && evenIndex < A.size())
+//                Swap(A[oddIndex], A[evenIndex]);
+//
+//            oddIndex++;
+//        }
+//
+//        return A;
+//    }
+//
+//    void Swap(int& a, int& b)
+//    {
+//        int temp = a;
+//        a = b;
+//        b = temp;
+//    }
+//};
+//
+//int main()
+//{
+//    Solution s;
+//    s.sortArrayByParity(vector<int> {2});
+//}
