4fd07c94c0b457f3be5b7c51ae4cbaf50739bcb9
JangMotti <totok682@naver.com>
2021-10-02 18:49:53 +0900

    LeetCode - Longest Palindromic Substring 풀이

--- Algorithms/LeetCode_LongestPalindromicSubstring.cpp
+++ Algorithms/LeetCode_LongestPalindromicSubstring.cpp
@@ -1,55 +1,63 @@
-//#include <string>
-//
-//using namespace std;
-//
-//class Solution {
-//private:
-//    string getLongestEqualString(string::iterator iter1, string::iterator end1, string::reverse_iterator iter2, string::reverse_iterator end2)
-//    {
-//        string maxS, s;
-//        while (iter1 != end1 && iter2 != end2)
-//        {
-//            if (*iter1 == *iter2)
-//            {
-//                s.push_back(*iter2);
-//                if (s.length() > maxS.length())
-//                    maxS = s;
-//            }
-//            else
-//                s.clear();
-//
-//            iter1++;
-//            iter2++;
-//        }
-//
-//        return maxS;
-//    }
-//public:
-//
-//    string longestPalindrome(string s) {
-//        string maxPalindrome, palindrome;
-//        auto iter = s.begin();
-//        auto riter = s.rbegin();
-//
-//        while (iter != s.end())
-//        {
-//            palindrome = getLongestEqualString(iter++, s.end(), s.rbegin(), s.rend());
-//            if (palindrome.length() > maxPalindrome.length())
-//                maxPalindrome = palindrome;
-//
-//            palindrome = getLongestEqualString(s.begin(), s.end(), riter++, s.rend());
-//            if (palindrome.length() > maxPalindrome.length())
-//                maxPalindrome = palindrome;
-//        }
-//
-//        return maxPalindrome;
-//    }
-//};
-//
-//int main()
-//{
-//    Solution s;
-//    auto result = s.longestPalindrome("aacabdkacaa");
-//
-//    return 0;
-//}
+#include <string>
+
+using namespace std;
+
+class Solution {
+private:
+	string getMaxPalindrome(const string& s, int l, int r)
+	{
+		while (l >= 0 && r < s.length())
+		{
+			if (s[l] != s[r])
+				break;
+			l--;
+			r++;
+		}
+		l++;
+
+		return s.substr(l, r - l);
+	}
+public:
+
+	string longestPalindrome(string s) {
+		string maxPalindrome;
+		if (s.empty())
+			return "";
+
+		maxPalindrome = s[0];
+
+		for (int i = 0; i < s.length() - 1; i++)
+		{
+			string p;
+			if (i + 1 < s.length() && s[i] == s[i + 1])
+			{
+				p = getMaxPalindrome(s, i, i + 1);
+				if (p.length() > maxPalindrome.length())
+					maxPalindrome = p;
+			}
+
+			if (i + 2 < s.length() && s[i] == s[i + 2])
+			{
+				p = getMaxPalindrome(s, i, i + 2);
+				if (p.length() > maxPalindrome.length())
+					maxPalindrome = p;
+			}
+		}
+
+		return maxPalindrome;
+	}
+};
+
+int main()
+{
+	Solution s;
+	auto result = s.longestPalindrome("aacabdkacaa");
+	auto result2 = s.longestPalindrome("aaaa");
+	auto result3 = s.longestPalindrome("123123454321");
+	auto result4 = s.longestPalindrome("1231221321");
+	auto result5 = s.longestPalindrome("1232111111");
+	auto result6 = s.longestPalindrome("1");
+	auto result7 = s.longestPalindrome("");
+
+	return 0;
+}
