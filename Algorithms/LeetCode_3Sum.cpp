408e36f6faff3a55cc50ce970dc155f59714027f
JangHyeonJun <totok682@naver.com>
2022-03-05 17:08:55 +0900

    LeetCode - 3Sum 시간초과

--- Algorithms/LeetCode_3Sum.cpp
+++ Algorithms/LeetCode_3Sum.cpp
@@ -2,26 +2,30 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_set>
+#include <string>
 
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-		sort(nums.begin(), nums.end());
-		unordered_set<tuple<int, int, int>> us;
-
+		unordered_set<string> us;
 		vector<vector<int>> result;
+		
+		sort(nums.begin(), nums.end());
+
 		for (int i = 0; i < nums.size() - 2; i++)
 			for (int j = i + 1; j < nums.size() - 1; j++)
 				for (int k = j + 1; k < nums.size(); k++)
 					if ((nums[i] + nums[j] + nums[k] == 0))
 					{
-						if (us.find({ nums[i],nums[j],nums[k] }) != us.end())
+						auto s = to_string(nums[i]) + to_string(nums[j]) + to_string(nums[k]);
+
+						if (us.find(s) != us.end())
 							continue;
 
+						us.insert(s);
 						result.push_back({ nums[i],nums[j],nums[k] });
-						us.insert({ nums[i],nums[j],nums[k] });
 					}
 		return result;
     }
@@ -32,7 +36,7 @@ int main()
 	Solution s;
 	vector<int> v = { -1, 0, 1, 2, -1, -4 };
 	vector<int> v2 = { -1, -1, -1, 0, 1, 1, 1 };
-	s.threeSum(v);
+	s.threeSum(v2);
 
 	return 0;
 }
