c2d8a837758662375b8793b029cdfa76d7a441eb
JangHyeonJun <totok682@naver.com>
2022-03-05 18:11:13 +0900

    LeetCode - 3Sum 시간초과(2)

--- Algorithms/LeetCode_3Sum.cpp
+++ Algorithms/LeetCode_3Sum.cpp
@@ -1,42 +1,76 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <unordered_set>
-#include <string>
-
-using namespace std;
-
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-		unordered_set<string> us;
-		vector<vector<int>> result;
-		
-		sort(nums.begin(), nums.end());
-
-		for (int i = 0; i < nums.size() - 2; i++)
-			for (int j = i + 1; j < nums.size() - 1; j++)
-				for (int k = j + 1; k < nums.size(); k++)
-					if ((nums[i] + nums[j] + nums[k] == 0))
-					{
-						auto s = to_string(nums[i]) + to_string(nums[j]) + to_string(nums[k]);
-
-						if (us.find(s) != us.end())
-							continue;
-
-						us.insert(s);
-						result.push_back({ nums[i],nums[j],nums[k] });
-					}
-		return result;
-    }
-};
-
-int main()
-{
-	Solution s;
-	vector<int> v = { -1, 0, 1, 2, -1, -4 };
-	vector<int> v2 = { -1, -1, -1, 0, 1, 1, 1 };
-	s.threeSum(v2);
-
-	return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <unordered_set>
+//#include <string>
+//
+//using namespace std;
+//
+//class Solution {
+//private:
+//	void SetResult(const vector<int>& nums, unordered_set<string>& us, vector<vector<int>>& result, int left, int center, int right)
+//	{
+//		string s = to_string(nums[left]) + to_string(nums[center]) + to_string(nums[right]);
+//
+//		if (us.find(s) == us.end())
+//		{
+//			us.insert(s);
+//			result.push_back({ nums[left], nums[center], nums[right] });
+//		}
+//	}
+//
+//	int GetThreeSumCenter(vector<int>& nums, int left, int right)
+//	{
+//		int sum = nums[left] + nums[right];
+//
+//		for (int i = left + 1; i < right; i++)
+//			if (sum + nums[i] == 0)
+//				return i;
+//		return -1;
+//	}
+//
+//public:
+//    vector<vector<int>> threeSum(vector<int>& nums) {
+//		unordered_set<string> us;
+//		vector<vector<int>> result;
+//		
+//		sort(nums.begin(), nums.end());
+//
+//		int leftOffSet = 0;
+//		int rightOffSet = nums.size() - 1;
+//
+//		while (leftOffSet < rightOffSet - 1)
+//		{
+//			for (int right = rightOffSet; right > leftOffSet + 1; right--)
+//			{
+//				auto center = GetThreeSumCenter(nums, leftOffSet, right);
+//				if (center != -1)
+//					SetResult(nums, us, result, leftOffSet, center, right);
+//			}
+//
+//			for (int left = leftOffSet + 1; left < rightOffSet - 1; left++)
+//			{
+//				auto center = GetThreeSumCenter(nums, left, rightOffSet);
+//				if (center != -1)
+//					SetResult(nums, us, result, left, center, rightOffSet);
+//			}
+//
+//			leftOffSet++;
+//			rightOffSet--;
+//		}
+//
+//		return result;
+//    }
+//};
+//
+//int main()
+//{
+//	Solution s;
+//	vector<int> v = { -1, 0, 1, 2, -1, -4 };
+//	vector<int> v2 = { -1, -1, -1, 0, 1, 1, 1 };
+//	vector<int> v3 = { -1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4 };
+//	vector<int> v4 = { -4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6 };
+//	s.threeSum(v4);
+//
+//	return 0;
+//}
