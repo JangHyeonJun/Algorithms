19ee8922b5b4dd91ed8987a19736f128677a0b35
JangHyeonJun <totok682@naver.com>
2022-04-30 17:21:55 +0900

    LeetCode - Generate Parentheses 풀이

---
+++ Algorithms/LeetCode_GenerateParentheses.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+private:
+
+    void dfs(vector<string>& result, int n, pair<int, int> count = make_pair(0,0), string s = "")
+    {
+        if (count.second == n)
+        {
+            result.push_back(s);
+            return;
+        }
+
+        if (count.first < n)
+            dfs(result, n, make_pair(count.first + 1, count.second), s + '(');
+
+        if (count.second < count.first)
+            dfs(result, n, make_pair(count.first, count.second + 1), s + ')');
+    }
+
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        this->dfs(result, n);
+
+        return result;
+    }
+};
+
+int main()
+{
+    Solution s;
+    auto result = s.generateParenthesis(1);
+
+    for (const auto& s : result)
+        cout << s << endl;
+	return 0;
+}
