40b8370f9030714d326378bba5fb91ac8d2f58de
JangHyeonJun <totok682@naver.com>
2022-11-02 20:41:02 +0900

    LeetCode - Divide Two Integers (시간초과)

---
+++ Algorithms/LeetCode_DivideTwoIntegers.cpp
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <limits>
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+
+        bool isPositiveQuotient = (dividend > 0 && divisor > 0) || (dividend < 0) && (divisor < 0);
+
+        long long absDividend = abs(dividend);
+        long long absDivisor = abs(divisor);
+
+        long long sum = 0;
+        long long quotient = 0;
+
+        while (absDividend >= sum)
+        {
+            sum += absDivisor;
+            quotient++;
+        }
+        quotient--;
+
+        if (quotient >= INT_MAX && isPositiveQuotient)
+            return INT_MAX;
+
+        if (-quotient <= INT_MIN && isPositiveQuotient)
+            return INT_MIN;
+
+        return isPositiveQuotient ? quotient : -quotient;
+    }
+};
