7bfa65dcbffdcdd25180b899c294731e3a813b2a
JangHyeonJun <totok682@naver.com>
2022-12-10 21:53:39 +0900

    LeetCode - Combination Sum 2 개선 - 로직 개선하여 인자갯수 줄임

--- Algorithms/LeetCode_CombinationSum2.cpp
+++ Algorithms/LeetCode_CombinationSum2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Solution {
 private:
-    void recursiveFind(const vector<int>& cand, int target, vector<vector<int>>& answer, vector<int>& comb, int i, int last)
+    void recursiveFind(const vector<int>& cand, int target, vector<vector<int>>& answer, vector<int>& comb, int i)
     {
         if (target == 0)
             answer.push_back(comb);
@@ -14,17 +14,16 @@ private:
 
         for (; i < cand.size(); i++)
         {
-            if (cand[i] == last)
-                continue;
-
             comb.push_back(cand[i]);
             target -= cand[i];
 
-            recursiveFind(cand, target, answer, comb, i + 1, last);
+            recursiveFind(cand, target, answer, comb, i + 1);
 
             target += cand[i];
             comb.pop_back();
-            last = cand[i];
+
+            while (i < cand.size() - 1 && cand[i] == cand[i + 1])
+                i++;
         }
     }
 
@@ -34,7 +33,7 @@ public:
 
         sort(candidates.begin(), candidates.end());
         vector<int> v;
-        recursiveFind(candidates, target, answer, v, 0, 0);
+        recursiveFind(candidates, target, answer, v, 0);
 
         return answer;
     }
@@ -44,8 +43,8 @@ int main()
 {
     Solution s;
 
-    vector<int> v{ 1, 1, 2, 3, 3, 5 };
-    int target = 7;
+    vector<int> v{ 1,1,1,1,1,1,2,2,2,2,2 };
+    int target = 9;
 
     s.combinationSum2(v, target);
 
