348119d1e81da688976941421c13f401c4adac18
JangHyeonJun <totok682@naver.com>
2022-12-25 18:18:27 +0900

    LeetCode - Pow(x, n) 분할정복기법 풀이 - x^n == (x^2)^n/2 임을 통해서 분할하여 풀이하는 방법 사용

--- Algorithms/LeetCode_Pow.cpp
+++ Algorithms/LeetCode_Pow.cpp
@@ -1,28 +1,29 @@
 #include <math.h>
 
 class Solution {
+
+  double divideConquerPow(double x, int n)
+  {
+    if (n == 0)
+      return 1;
+    else if (n == 1)
+      return x;
+    else if (n % 2 == 0)
+      return divideConquerPow(x * x, n / 2);
+    else
+      return x * divideConquerPow(x * x, n / 2);
+  }
 public:
   double myPow(double x, int n) {
-    double multiple = x;
-    long square = abs(n);
-
-    if (square == 0 || x == 1.00000)
-      return 1;
-    else if (x == -1.00000 && square % 2 == 0)
-      return 1;
-    else if (x == -1.00000 && square % 2 != 0)
-      return -1;
-
-    for (int i = 0; i < square - 1; i++)
-    {
-      if (x >= 1000000)
-        break;
-      x *= multiple;
-    }
-
-    if (n >= 0)
-      return x;
-
-    return 1 / x;
+    return n >= 0 ? divideConquerPow(x, n) : 1 / divideConquerPow(x, n);
   }
 };
+
+int main()
+{
+  Solution s;
+  //auto result = s.myPow(8.84372, -5);
+  auto result2 = s.myPow(2, 10);
+
+  return 0;
+}
