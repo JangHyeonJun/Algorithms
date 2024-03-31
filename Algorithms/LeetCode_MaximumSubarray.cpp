6c66a77eda66f6dbde3f1186b1e04b96b8a0d41c
JangHyeonJun <totok682@naver.com>
2024-03-31 17:35:08 +0900

    LeetCode - MaximumSubArray 브루트포스 풀이

--- Algorithms/LeetCode_MaximumSubarray.cpp
+++ Algorithms/LeetCode_MaximumSubarray.cpp
@@ -1,31 +1,42 @@
-//#include <vector>
-//#include <math.h>
-//#include <algorithm>
-//
-//using namespace std;
-//
-//class Solution {
-//public:
-//  int maxSubArray(vector<int>& nums) {
-//    int allMax = nums[0], sum = 0;
-//
-//    for (int i = 0; i < nums.size(); i++)
-//    {
-//      sum += nums[i];
-//      sum = max(sum, nums[i]);
-//      allMax = max(allMax, sum);
-//    }
-//
-//    return allMax;
-//  }
-//};
-//
-//int main()
-//{
-//  Solution s;
-//  vector<int> v{ -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-//  vector<int> v2{ 0, -1, -2, -3 };
-//  auto result = s.maxSubArray(v);
-//
-//  return 0;
-//}
+#include <vector>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+  int maxSubArray(vector<int>& nums) {
+    int sum, maxSum;
+    maxSum = INT_MIN;
+
+    vector<int> v(nums.size());
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+      sum = 0;
+      v.clear();
+      for (int j = i; j < nums.size(); j++)
+      {
+        sum += nums[j];
+        if (sum > maxSum)
+          maxSum = sum;
+      }
+
+    }
+
+    return maxSum;
+  }
+};
+
+int main()
+{
+  Solution s;
+  vector<int> v{ -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+  vector<int> v2{ -2,1,-3,4,-1,2,1,-5, 4 };
+  vector<int> v3{ 1,2,-1,-2,2,1,-2,1 };
+  vector<int> v4{ 8, -19, 5, -4, 20 };
+  auto result = s.maxSubArray(v4);
+
+  return 0;
+}
