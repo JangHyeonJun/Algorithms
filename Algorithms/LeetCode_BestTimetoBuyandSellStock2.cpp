18a8a0c25641cb5da2a4f3a14635be8aecfb3414
JangHyeonJun <totok682@naver.com>
2025-03-19 22:19:52 +0900

    LeetCode - Best Time to Buy and Sell Stock 리팩토링

--- Algorithms/LeetCode_BestTimetoBuyandSellStock2.cpp
+++ Algorithms/LeetCode_BestTimetoBuyandSellStock2.cpp
@@ -1,28 +1,16 @@
 #include <vector>
-
 using namespace std;
 
 class Solution {
 public:
 	int maxProfit(vector<int>& prices) {
-		vector<int> tmp(prices.size());
-		int min = prices[0];
-		int max = prices[prices.size() - 1];
-		int result = 0;
+		int min = 2147483647, result = 0;
 		for (int i = 0; i < prices.size(); i++)
 		{
 			if (prices[i] < min)
 				min = prices[i];
-			tmp[i] = min;
-		}
-
-		for (int j = prices.size() - 1; j >= 0; j--)
-		{
-			if (prices[j] > max)
-				max = prices[j];
-
-			if (max - tmp[j] > result)
-				result = max - tmp[j];
+			if (prices[i] - min > result)
+				result = prices[i] - min;
 		}
 
 		return result;
