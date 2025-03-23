bc4e5ec6c69e35d04fc1065c0727dfc2be67a49b
JangHyeonJun <totok682@naver.com>
2025-03-23 16:23:18 +0900

    LeetCode - Power of Three 풀이

---
+++ Algorithms/LeetCode_PowerOfThree2.cpp
@@ -0,0 +1,15 @@
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int max_3square = 1162261467;
+//
+//class Solution {
+//    vector<int> v{ 1,3,9,7 };
+//public:
+//    bool isPowerOfThree(int n) {
+//        return n > 0 && max_3square % n == 0;
+//    }
+//};
+//
+//// NOTE: 반복문 없이 푸는건 몰라서 인터넷 찾아봄..
