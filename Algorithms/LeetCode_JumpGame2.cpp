78efc89ab15c8cfe6552bb77abd9e263b7f1ffb8
JangHyeonJun <totok682@naver.com>
2025-04-06 17:46:07 +0900

    LeetCode - Jump Game 두번째 풀이 - 그리디 풀이

--- Algorithms/LeetCode_JumpGame2.cpp
+++ Algorithms/LeetCode_JumpGame2.cpp
@@ -1,40 +1,22 @@
 //#include <vector>
-//#include <stack>
+//#include <algorithm>
 //using namespace std;
 //
 //class Solution {
 //public:
 //	bool canJump(vector<int>& nums) {
-//		stack<int> s;
-//		vector<bool> flags(nums.size(), false);
-//		s.push(0);
+//		int maxIndex = nums[0];
+//		for (int i = 0; i <= maxIndex && i < nums.size(); i++)
+//			maxIndex = max(maxIndex, i + nums[i]);
 //
-//		do
-//		{
-//			int index = s.top();
-//			s.pop();
-//
-//
-//			if (index >= nums.size() - 1)
-//				return true;
-//
-//			if (flags[index])
-//				continue;
-//
-//			flags[index] = true;
-//
-//			for (int i = 1; i <= nums[index]; i++)
-//				s.push(index + i);
-//
-//		} while (s.empty() == false);
-//
-//		return false;
+//		return maxIndex >= nums.size() - 1;
 //	}
 //};
 //
 //int main()
 //{
 //	Solution s;
-//	vector<int> v{ 2,3,1,1,4 };
-//	s.canJump(v);
+//	vector<int> v{ 1,2,3 };
+//	auto result = s.canJump(v);
+//	return 0;
 //}
