34f6a4e9ad8063828375ba657496bf20a5080e28
JangHyeonJun <totok682@naver.com>
2025-08-27 22:42:29 +0900

    feat: LeetCode - Binary Tree Inorder Traversal 풀이

---
+++ Algorithms/LeetCode_BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,37 @@
+﻿#include <vector>
+#include <stack>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s;
+        vector<int> v;
+
+        auto top = root;
+        while (top != nullptr || !s.empty())
+        {
+            while (top != nullptr)
+            {
+                s.push(top);
+                top = top->left;
+            }
+
+            top = s.top();
+            s.pop();
+            v.push_back(top->val);
+            top = top->right;
+        }
+
+        return v;
+    }
+};
