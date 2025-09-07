24e966ddcb03506585da3ca93b84dc2cc9cf300c
JangHyeonJun <totok682@naver.com>
2025-09-07 13:18:23 +0900

    LeetCode - CoinChange, CloneGraph 풀이

---
+++ Algorithms/LeetCode_CloneGraph.cpp
@@ -0,0 +1,62 @@
+﻿//#include <vector>
+//#include <stack>
+//#include <unordered_map>
+//using namespace std;
+//
+//// Definition for a Node.
+//class Node {
+//public:
+//    int val;
+//    vector<Node*> neighbors;
+//    Node() {
+//        val = 0;
+//        neighbors = vector<Node*>();
+//    }
+//    Node(int _val) {
+//        val = _val;
+//        neighbors = vector<Node*>();
+//    }
+//    Node(int _val, vector<Node*> _neighbors) {
+//        val = _val;
+//        neighbors = _neighbors;
+//    }
+//};
+//
+//
+//class Solution {
+//public:
+//    Node* cloneGraph(Node* node) {
+//        if (node == nullptr)
+//            return node;
+//
+//        unordered_map<Node*, Node*> um;
+//        stack<Node*> st;
+//
+//        um[node] = new Node(node->val);
+//        st.push(node);
+//
+//        while (!st.empty()) {
+//            auto curr = st.top();
+//            st.pop();
+//
+//            auto clone = um[curr];
+//
+//            vector<Node*> v;
+//            for (auto neighbor : curr->neighbors)
+//            {
+//                if (um.find(neighbor) == um.end())
+//                {
+//                    um[neighbor] = new Node(neighbor->val);
+//                    st.push(neighbor);
+//                }
+//
+//                auto cloneNeighbor = um[neighbor];
+//                v.push_back(cloneNeighbor);
+//            }
+//
+//            clone->neighbors = v;
+//        }
+//
+//        return um[node];
+//    }
+//};
---
+++ Algorithms/LeetCode_CoinChange.cpp
@@ -0,0 +1,39 @@
+﻿//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//class Solution {
+//public:
+//    int coinChange(vector<int>& coins, int amount) {
+//        if (amount == 0)
+//            return 0;
+//
+//        vector<long long> v(amount + 1, 0);
+//
+//        for (auto n : coins)
+//            if (n == amount)
+//                return 1;
+//            else if (n < amount)
+//                v[n]++;
+//
+//        for (long long i = 1; i <= amount; i++)
+//        {
+//            if (v[i] == 0)
+//                continue;
+//
+//            for (auto n : coins)
+//            {
+//                long long next = i + n;
+//                if (next > amount)
+//                    continue;
+//
+//                if (v[next] == 0)
+//                    v[next] = v[i] + 1;
+//                else
+//                    v[next] = v[next] < v[i] + 1 ? v[next] : v[i] + 1;
+//            }
+//        }
+//
+//        return v[amount] != 0 ? v[amount] : -1;
+//    }
+//};
