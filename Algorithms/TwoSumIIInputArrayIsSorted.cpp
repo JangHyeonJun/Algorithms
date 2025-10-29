60635cf65e3c9d6348eadcfdfd821f5e6c57487e
JangHyeonJun <totok682@naver.com>
2025-10-29 18:02:57 +0900

    LeetCode - Two Sum II - Input Array Is Sorted 풀이

---
+++ Algorithms/TwoSumIIInputArrayIsSorted.cpp
@@ -0,0 +1,21 @@
+﻿//#include <vector>
+//using namespace std;
+//
+//class Solution {
+//public:
+//    vector<int> twoSum(vector<int>& numbers, int target) {
+//        int head = 0, tail = numbers.size() - 1;
+//        while (head < tail)
+//        {
+//            auto sum = numbers[head] + numbers[tail];
+//            if (sum == target)
+//                return { head + 1, tail + 1 };
+//            else if (sum < target)
+//                head++;
+//            else
+//                tail--;
+//        }
+//
+//        return {-1, -1};
+//    }
+//};
