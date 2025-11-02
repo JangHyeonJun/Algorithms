733113bcb9a7b8b13085cbb020a4e652f68c642a
JangHyeonJun <totok682@naver.com>
2025-11-02 15:50:24 +0900

    LeetCode - Longest Consecutive Sequence 풀이 - HashSet 사용해야하는건 맞고, hashset을 한번만 순회하면서 중복되지않은 내부순환(작은수부터 연속되는 숫자 있는지 체크)은 OK

---
+++ Algorithms/LeetCode_LongestConsecutiveSequence.cpp
@@ -0,0 +1,30 @@
+﻿//#include <vector>
+//#include <unordered_set>
+//using namespace std;
+//
+//class Solution {
+//public:
+//    int longestConsecutive(vector<int>& nums) {
+//        unordered_set<int> us;
+//        int min = 987654321;
+//        int result = 0;
+//
+//        for (auto n : nums)
+//            us.insert(n);
+//
+//        for (auto n : us)
+//        {
+//            if (us.contains(n - 1))
+//                continue;
+//
+//            int count = 0;
+//            while (us.contains(n++))
+//                count++;
+//
+//            if (count > result)
+//                result = count;
+//        }
+//
+//        return result;
+//    }
+//};
