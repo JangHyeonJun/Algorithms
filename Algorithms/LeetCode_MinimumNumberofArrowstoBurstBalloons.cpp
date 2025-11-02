2e72c313b288739e7adef009458fef3aaa8ffaa0
JangHyeonJun <totok682@naver.com>
2025-11-02 19:39:54 +0900

    LeetCode - Minimum Number of Arrows to Burst Balloons 풀이
    - Greedy 풀이 (범위, 겹침)

---
+++ Algorithms/LeetCode_MinimumNumberofArrowstoBurstBalloons.cpp
@@ -0,0 +1,33 @@
+﻿//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//
+//class Solution {
+//public:
+//    int findMinArrowShots(vector<vector<int>>& points) {
+//        sort(points.begin(), points.end(), [](const auto& a, const auto& b) { return a[1] < b[1]; });
+//
+//        int result = 0;
+//        vector<int> prev;
+//        for (auto v : points)
+//            if (!prev.empty() && prev[1] >= v[0])
+//                continue;
+//            else
+//            {
+//                result++;
+//                prev = v;
+//            }
+//
+//        return result;
+//    }
+//};
+//
+//int main()
+//{
+//    Solution s;
+//    vector<vector<int>> v = { {3,9},{7,12},{3,8},{6,8},{9,10},{2,9},{0,9},{3,9},{0,6},{2,8} };
+//    s.findMinArrowShots(v);
+//
+//    return 0;
+//}
